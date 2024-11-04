25ee3c2bcac6e6255c805998b086c9d1cf4e5935
meta-ke <grizzly_bear2021@163.com>
2024-11-04 21:18:28 +0800

    init

---
+++ device/edge.py
@@ -0,0 +1,128 @@
+# --------------------------------------------------------
+# Tensorflow Faster R-CNN
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Jiasen Lu, Jianwei Yang, based on code from Ross Girshick
+# --------------------------------------------------------
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import base64
+import os
+import sys
+import numpy as np
+import argparse
+import pprint
+import pdb
+import time
+import cv2
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.optim as optim
+import pickle
+import pdb
+from torchvision import transforms
+from ecci_sdk_cloud import Client
+import threading
+
+try:
+    xrange          # Python 2
+except NameError:
+    xrange = range  # Python 3
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+
+def parse_args():
+    """
+    Parse input arguments
+    """
+    parser = argparse.ArgumentParser(description='Train a Fast R-CNN network')
+    parser.add_argument('--arch', dest='arch', default='rfcn', choices=['rcnn', 'rfcn', 'couplenet'])
+    parser.add_argument('--dataset', dest='dataset',
+                        help='training dataset',
+                        default='imagenet_vid', type=str)
+    parser.add_argument('--cfg', dest='cfg_file',
+                        help='optional config file',
+                        default='cfgs/res16.yml', type=str)
+    parser.add_argument('--net', dest='net',
+                        help='vgg16, res50, res101, res152',
+                        default='res101', type=str)
+    parser.add_argument('--set', dest='set_cfgs',
+                        help='set config keys', default=None,
+                        nargs=argparse.REMAINDER)
+    parser.add_argument('--load_dir', dest='load_dir',
+                        help='directory to load models', default="models",
+                        type=str)
+    parser.add_argument('--cuda', dest='cuda',
+                        help='whether use CUDA',
+                        action='store_true')
+    parser.add_argument('--ls', dest='large_scale',
+                        help='whether use large imag scale',
+                        action='store_true')
+    parser.add_argument('--mGPUs', dest='mGPUs',
+                        help='whether use multiple GPUs',
+                        action='store_true')
+    parser.add_argument('--cag', dest='class_agnostic',
+                        help='whether perform class_agnostic bbox regression',
+                        action='store_true')
+    parser.add_argument('--parallel_type', dest='parallel_type',
+                        help='which part of model to parallel, 0: all, 1: model before roi pooling',
+                        default=0, type=int)
+    parser.add_argument('--checksession', dest='checksession',
+                        help='checksession to load model',
+                        default=1, type=int)
+    parser.add_argument('--checkepoch', dest='checkepoch',
+                        help='checkepoch to load network',
+                        default=100, type=int)
+    parser.add_argument('--checkpoint', dest='checkpoint',
+                        help='checkpoint to load network',
+                        default=1036, type=int)
+    parser.add_argument('--bs', dest='batch_size',
+                        help='batch_size',
+                        default=1, type=int)
+    parser.add_argument('--vis', dest='vis',
+                        help='visualization mode',
+                        action='store_true')
+    parser.add_argument('--load_name',
+                        default="./models/detect.pth",
+                        help='load checkpoint',
+                        )
+    args = parser.parse_args()
+    return args
+    # lr = cfg.TRAIN.LEARNING_RATE
+    # momentum = cfg.TRAIN.MOMENTUM
+    # weight_decay = cfg.TRAIN.WEIGHT_DECAY
+if __name__ == '__main__':
+    # Initialize ecci sdk and connect to the broker in edge-cloud
+    ecci_client = Client()
+    mqtt_thread = threading.Thread(target=ecci_client.initialize)
+    mqtt_thread.start()
+    ecci_client.wait_for_ready()
+    print('cloud start --------')
+
+    args = parse_args()
+
+    # if args.arch == 'rcnn':
+    #     from model.faster_rcnn.vgg16 import vgg16
+    #     from model.faster_rcnn.resnet import resnet
+    # elif args.arch == 'rfcn':
+    #     from model.rfcn.resnet_atrous import resnet
+    # elif args.arch == 'couplenet':
+    #     from model.couplenet.resnet_atrous import resnet
+
+    print('Called with args:')
+    print(args)
+
+    if torch.cuda.is_available() and not args.cuda:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+    while True:
+        edge_data = ecci_client.get_sub_data_payload_queue().get()
+        frame1 = edge_data["frame1"]
+        img = base64.b64decode(frame1)
+        npimg = np.frombuffer(img, dtype=np.uint8)
+        source = cv2.imdecode(npimg, 1)
+        print("ss")
+        # cv2.imshow("stream", source)
+        # cv2.waitKey(0)
---
+++ devices1.py
@@ -0,0 +1,119 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import base64
+import os
+import argparse
+
+import cv2
+import torch
+import numpy as np
+import glob
+import pickle
+import pathlib
+from ecci_sdk_edge_1 import Client
+import multiprocessing as mp
+import threading, random, socket, socketserver, time, pickle
+from datasets_data.vid_dataset import ImagenetDataset
+from datasets_data.data_preprocessing import group_annotation_by_class
+
+parser = argparse.ArgumentParser(description='tracking demo')
+parser.add_argument("--dataset", type=str, default="G:/baidupandownload/kubeedge_meeting/dataset/imagenet2015/ILSVRC/",
+                    help="The root directory of the VOC dataset or Open Images dataset.")
+parser.add_argument('--config', type=str, help='config file', default="./experiments/siamrpn_alex_dwxcorr_multi/config.yaml")
+parser.add_argument('--snapshot', type=str, help='model name', default="./experiments/siamrpn_alex_dwxcorr_multi/track.pth")
+parser.add_argument("--label_file", type=str, default='./datasets/vid-model-labels.txt', help="The label file path.")
+parser.add_argument("--eval_dir", default="./eval/output/", type=str,
+                    help="The directory to store evaluation results.")
+
+args = parser.parse_args()
+def main():
+# Initialize ecci sdk and connect to the broker in edge-cloud
+    ecci_client = Client()
+    mqtt_thread = threading.Thread(target=ecci_client.initialize)
+    mqtt_thread.start()
+    ecci_client.wait_for_ready()
+    print('edge start --------')
+
+    # load config
+    # cfg.merge_from_file(args.config)
+    # cfg.CUDA = torch.cuda.is_available() and cfg.CUDA
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    dataset = ImagenetDataset(args.dataset, is_val=True)
+    #true_case_stat, all_gb_boxes = group_annotation_by_class(dataset)
+    #frame = cv2.
+    T1 = time.time()
+    T2 = time.time()
+    for f in range(len(dataset)):
+        # print("sdsadsd", f)
+        edge_data = ecci_client.get_sub_data_payload_queue().get()
+        data_flag = edge_data["frame"]
+        print("data_flag", data_flag)
+        if data_flag == 'start':
+            stat_time = time.time()
+            bz_num = 1
+            frame_data = []
+            for i in range(bz_num):
+                stat_time = time.time()
+                frame, first_frame = dataset.get_image(f)
+                frame = cv2.resize(frame, (224, 224), fx=1.2, fy=1.6, interpolation=cv2.INTER_CUBIC)
+                # cv2.imshow("source", frame)
+                # cv2.waitKey(0)
+                # frame1, frame2, frame3 = np.split(frame, 3)
+                print("type", type(frame))
+                T3 = time.time()
+                print("time1111:", T3 - T2)
+                # frame_data.append(frame)
+                #frame_image = frame.array
+            encoded, buffer = cv2.imencode('.jpg', frame)
+
+                # print("buffer")
+
+            jpg_as_text = base64.b64encode(buffer)
+
+            # jpg_as_time = base64.b64encode(stat_time)
+            print("jpg_as_text")
+
+
+            # send frame to cloud
+            payload = {"type": "data", "contents": {"frame1": jpg_as_text, "time1": stat_time, "bz": bz_num}}
+            #print("####################", payload)
+            ecci_client.publish(payload, "cloud1")
+
+            # frame1 = edge_data["frame1"]
+            # payload = {"type": "data", "contents": {"frame2": frame2}}
+            # #print("####################", payload)
+            # ecci_client.publish(payload, "cloud")
+            # payload = {"type": "data", "contents": {"frame3": frame3}}
+            # #print("####################", payload)
+            # ecci_client.publish(payload, "cloud")
+
+            # get rect from cloud
+            # cloud_data = ecci_client.get_sub_data_payload_queue().get()
+            # print("ss")
+            # #print("###########recieve data from cloud", cloud_data)
+            # bbox = cloud_data["bbox"]
+            # label = cloud_data["label"]
+            # probs = cloud_data["probs"]
+            T2 = time.time()
+            print("time:", T2-T1)
+            T1=T2
+
+
+    payload = {"type": "data", "contents": {"frame1": "end"}}
+    # print("####################", payload)
+    ecci_client.publish(payload, "cloud1")
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        mp.set_start_method('spawn')
+    except RuntimeError:
+        pass
+    main()
